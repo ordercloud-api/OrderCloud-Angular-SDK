31f47b346d537c000cbdd808c9236d543c88cffd
Crhistian Ramirez <cramirez@four51.com>
2020-01-25 18:59:26 -0600

    :sparkles: add ability to configure cookie options

--- dist/ordercloud-angular-sdk.js
+++ dist/ordercloud-angular-sdk.js
@@ -26,7 +26,7 @@ angular.module('ordercloud-angular-sdk', [
 
 function OrderCloudService($cookies, $rootScope, $q) {
     var sdk = {},
-        authTokenCookieName, impersonationTokenCookieName, refreshTokenCookieName,
+        _authTokenCookieName, _impersonationTokenCookieName, _refreshTokenCookieName, _ngCookieOptions,
         defaultClient = ocSDK.Sdk.instance,
         oauth2 = defaultClient.authentications['oauth2'];
     
@@ -74,59 +74,66 @@ function OrderCloudService($cookies, $rootScope, $q) {
         }
     }
 
-    var _config = function(cookiePrefix, apiurl, authurl) {
-        authTokenCookieName = cookiePrefix + '.token';
-        impersonationTokenCookieName = cookiePrefix + '.impersonation.token';
-        refreshTokenCookieName = cookiePrefix + '.refresh.token';
+    /**
+     * @param {string} cookiePrefix - prepended to all cookie names set by the sdk
+     * @param {string = 'https://api.ordercloud.io/v1'} apiurl - the base path for ordercloud requests
+     * @param {string = 'https://auth.ordercloud.io/oauth/token'} authurl - the path that will be used to authenticate to the ordercloud api
+     * @param {object} ngCookieOptions - inherited from angular's [$cookies options](https://docs.angularjs.org/api/ngCookies/provider/$cookiesProvider#defaults)
+     */
+    var _config = function(cookiePrefix, apiurl, authurl, ngCookieOptions) {
+        _authTokenCookieName = cookiePrefix + '.token';
+        _impersonationTokenCookieName = cookiePrefix + '.impersonation.token';
+        _refreshTokenCookieName = cookiePrefix + '.refresh.token';
         if (apiurl !== null) {
             ocSDK.Sdk.instance.baseApiPath = apiurl;
         }
         if (authurl !== null) {
             ocSDK.Sdk.instance.baseAuthPath = authurl;
         }
+        _ngCookieOptions = ngCookieOptions;
         _getToken();
         _getImpersonationToken();
     }
 
     var _getToken = function() {
-        var token = $cookies.get(authTokenCookieName);
+        var token = $cookies.get(_authTokenCookieName);
         oauth2.accessToken = token;
         return token;
     };
 
     var _removeToken = function() {
-        $cookies.remove(authTokenCookieName);
+        $cookies.remove(_authTokenCookieName);
         oauth2.accessToken = null;
     };
 
     var _getImpersonationToken = function() {
-        var token = $cookies.get(impersonationTokenCookieName);
+        var token = $cookies.get(_impersonationTokenCookieName);
         oauth2.impersonationToken = token;
         return token;
     };
 
     var _removeImpersonationToken = function() {
-        $cookies.remove(impersonationTokenCookieName);
+        $cookies.remove(_impersonationTokenCookieName);
         oauth2.impersonationToken = null;
     }
 
     var _getRefreshToken = function() {
-        var token = $cookies.get(refreshTokenCookieName);
+        var token = $cookies.get(_refreshTokenCookieName);
         return token;
     }
 
     var _setToken = function(token) {
         oauth2.accessToken = token;
-        $cookies.put(authTokenCookieName, token);
+        $cookies.put(_authTokenCookieName, token, _ngCookieOptions);
     };
 
     var _setImpersonationToken = function(token) {
         oauth2.impersonationToken = token;
-        $cookies.put(impersonationTokenCookieName, token);
+        $cookies.put(_impersonationTokenCookieName, token, _ngCookieOptions);
     };
 
     var _setRefreshToken = function(token) {
-        $cookies.put(refreshTokenCookieName, token);
+        $cookies.put(_refreshTokenCookieName, token, _ngCookieOptions);
     }
 
     sdk.Config = _config;
